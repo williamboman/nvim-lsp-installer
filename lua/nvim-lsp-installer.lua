9d8af1b27fd6cf3ee783de150a844e79798afa32
William Boman <william@redwill.se>
2021-04-04 17:57:48 +0200

    update README and change get_installed_servers() return value

--- lua/nvim-lsp-installer.lua
+++ lua/nvim-lsp-installer.lua
@@ -2,7 +2,6 @@ local installer = require('nvim-lsp-installer.installer')
 
 local M = {}
 
-M.get_server_installer = installer.get_server_installer
 M.get_available_servers = installer.get_available_servers
 M.get_installed_servers = installer.get_installed_servers
 M.get_uninstalled_servers = installer.get_uninstalled_servers
