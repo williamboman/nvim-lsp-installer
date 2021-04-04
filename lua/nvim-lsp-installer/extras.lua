9ec5703ece269d41e00bb803a7a5464edde61d30
William Boman <william@redwill.se>
2021-04-04 18:54:02 +0200

    no goto

--- lua/nvim-lsp-installer/extras.lua
+++ lua/nvim-lsp-installer/extras.lua
@@ -2,9 +2,9 @@ local M = {}
 
 function M.send_client_request(client_name, ...)
     for _, client in pairs(vim.lsp.get_active_clients()) do
-        if client.name ~= client_name then goto continue end
-        client.request(...)
-        ::continue::
+        if client.name == client_name then
+            client.request(...)
+        end
     end
 end
 
