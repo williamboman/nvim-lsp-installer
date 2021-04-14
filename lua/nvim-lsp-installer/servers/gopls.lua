5eb4be95b86f3c558eef0b815ec8041fba60f917
ecmma <ecmma@anche.no>
2021-04-14 21:09:25 +0200

    gopls: add pre_install_check

--- lua/nvim-lsp-installer/servers/gopls.lua
+++ lua/nvim-lsp-installer/servers/gopls.lua
@@ -3,13 +3,6 @@ local server = require('nvim-lsp-installer.server')
 local root_dir = server.get_server_root_path('go')
 
 local install_cmd = [=[
-if ! command -v go &> /dev/null;
-then 
-  echo "Please install the Go CLI before installing gopls.";
-  echo "refer to https://golang.org/doc/install";
-  exit 1;
-fi
-
 GO111MODULE=on go get golang.org/x/tools/gopls@latest;
 
 if ! command -v gopls &> /dev/null; 
@@ -24,6 +17,11 @@ fi
 return server.Server:new {
   name = "gopls",
   root_dir = root_dir,
+  pre_install_check = function ()
+    if vim.fn.executable("go") ~= 1 then
+      error("Please install the Go CLI before installing gopls (https://golang.org/doc/install).")
+    end
+  end,
   install_cmd = install_cmd,
   default_options = {
     cmd = {"gopls"},
