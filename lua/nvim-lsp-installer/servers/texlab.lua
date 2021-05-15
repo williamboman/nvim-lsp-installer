a0dcadafa2195a4c723fcad2a8b373a99a8069fd
Edoardo Marangoni <ecmma@anche.no>
2021-05-15 22:19:58 +0200

    texlive: add wget and pdflatex checks

--- lua/nvim-lsp-installer/servers/texlab.lua
+++ lua/nvim-lsp-installer/servers/texlab.lua
@@ -20,6 +20,13 @@ return server.Server:new {
   name = "texlab", 
   root_dir = root_dir, 
   install_cmd = install_cmd, 
+  pre_install_check = function ()
+    if vim.fn.executable("wget") ~=1 then 
+      error ("Missing wget. Please, refer to https://www.gnu.org/software/wget/ to install it.")
+    elseif vim.fn.executable("pdflatex") ~=1 then
+      error("The program pdflatex wasn't found. Please install a TeX distribution: https://www.latex-project.org/get/#tex-distributions")
+    end
+  end,
   default_options = {
     cmd = {root_dir .. '/texlab'},
   }
