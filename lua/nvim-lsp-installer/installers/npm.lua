c1f1fa0b6be4b480578316f0af738887229890c7
William Boman <william@redwill.se>
2021-06-08 11:19:03 +0200

    installers/npm: add option to install via yarn instead

--- lua/nvim-lsp-installer/installers/npm.lua
+++ lua/nvim-lsp-installer/installers/npm.lua
@@ -3,7 +3,13 @@ local shell = require("nvim-lsp-installer.installers.shell")
 local M = {}
 
 function M.packages(packages)
-    return shell.raw(("npm install %s"):format(table.concat(packages, " ")))
+    return function (server, callback)
+        if vim.g.lsp_installer_use_yarn then
+            shell.raw(("yarn add %s"):format(table.concat(packages, " ")))(server, callback)
+        else
+            shell.raw(("npm install %s"):format(table.concat(packages, " ")))(server, callback)
+        end
+    end
 end
 
 return M
