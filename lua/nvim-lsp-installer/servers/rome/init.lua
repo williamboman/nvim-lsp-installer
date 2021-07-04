9296d533b0b4e1edf28816b2c3b9620fb4a16c79
William Boman <william@redwill.se>
2021-07-05 00:00:35 +0200

    use nightly version

--- lua/nvim-lsp-installer/servers/rome/init.lua
+++ lua/nvim-lsp-installer/servers/rome/init.lua
@@ -7,7 +7,7 @@ local root_dir = server.get_server_root_path("rome")
 return server.Server:new {
     name = "rome",
     root_dir = root_dir,
-    installer = npm.packages { "rome" },
+    installer = npm.packages { "rome@10.0.7-nightly.2021.7.2" }, -- https://github.com/rome/tools/pull/1409
     default_options = {
         cmd = { path.concat { root_dir, "node_modules", ".bin", "rome" }, "lsp" },
     },
