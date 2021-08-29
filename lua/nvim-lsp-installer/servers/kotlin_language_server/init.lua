d893f34fedce21d933cd16b4bc633092c37041c6
William Boman <william@redwill.se>
2021-08-29 17:30:04 +0200

    kotlin: fix cmd on windows

--- lua/nvim-lsp-installer/servers/kotlin_language_server/init.lua
+++ lua/nvim-lsp-installer/servers/kotlin_language_server/init.lua
@@ -1,6 +1,7 @@
 local server = require "nvim-lsp-installer.server"
 local path = require "nvim-lsp-installer.path"
 local installers = require "nvim-lsp-installer.installers"
+local platform = require "nvim-lsp-installer.platform"
 local shell = require "nvim-lsp-installer.installers.shell"
 
 local root_dir = server.get_server_root_path "kotlin"
@@ -17,6 +18,13 @@ return server.Server:new {
         win = shell.cmd [[ curl -fLo server.zip https://github.com/fwcd/kotlin-language-server/releases/latest/download/server.zip && tar -xvf server.zip && del /f server.zip ]],
     },
     default_options = {
-        cmd = { path.concat { root_dir, "server", "bin", "kotlin-language-server" } },
+        cmd = {
+            path.concat {
+                root_dir,
+                "server",
+                "bin",
+                platform.is_win() and "kotlin-language-server.bat" or "kotlin-language-server",
+            },
+        },
     },
 }
