17382cf9efa9d184b5fb7365b8b11bd6fc7f078d
aspeddro <pdesacastro@gmail.com>
2021-09-02 18:34:53 -0300

    add rescriptls

---
+++ lua/nvim-lsp-installer/servers/rescriptls/init.lua
@@ -0,0 +1,21 @@
+local server = require("nvim-lsp-installer.server")
+local path = require("nvim-lsp-installer.path")
+local shell = require("nvim-lsp-installer.installers.shell")
+
+local root_dir = server.get_server_root_path('rescriptls')
+
+return server.Server:new {
+    name = "rescriptls",
+    root_dir = root_dir,
+    installer = shell.polyshell [[
+        curl -s https://api.github.com/repos/rescript-lang/rescript-vscode/releases/latest \
+              | grep "browser_download_url.*vsix" \
+              | cut -d : -f 2,3 \
+              | tr -d '"' \
+              | wget -i - -O vscode-rescript.vsix;
+        unzip -q -o vscode-rescript.vsix;
+    ]],
+    default_options = {
+        cmd = {"node", path.concat { root_dir, "extension", "server", "out", "server.js" }, "--stdio"}
+    }
+}
