2b7c554122467afb0034fd37fe8b7968b0e8510a
aspeddro <pdesacastro@gmail.com>
2021-09-08 19:46:15 -0300

    install local server

--- lua/nvim-lsp-installer/servers/r_language_server/init.lua
+++ lua/nvim-lsp-installer/servers/r_language_server/init.lua
@@ -1,16 +1,21 @@
 local server = require 'nvim-lsp-installer.server'
-local shell = require "nvim-lsp-installer.installers.shell"
 local path = require "nvim-lsp-installer.path"
+local process = require("nvim-lsp-installer.process")
 
 local root_dir = server.get_server_root_path("r_language_server")
 
 return server.Server:new {
     name = "r_language_server",
     root_dir = root_dir,
-    installer = shell.polyshell([[R -e 'install.packages("languageserver", repos="http://cran.us.r-project.org")']]),
+    installer = function(server, callback, context)
+        process.spawn("R", {
+          args = { "-e", 'install.packages("languageserver", repos="http://cran.us.r-project.org", lib = "' .. root_dir .. '")' },
+          stdio_sink = context.stdio_sink
+        }, callback)
+    end,
     default_options = {
         cmd = {
-            'R', '--slave', '-e', 'languageserver::run()'
+            'R', '--slave', '-e', 'library(languageserver, lib.loc = "' .. root_dir .. '"); languageserver::run()'
         }
     }
 }
