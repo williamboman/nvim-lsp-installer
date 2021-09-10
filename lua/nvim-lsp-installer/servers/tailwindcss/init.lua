bf3581cc1070b0967e2f51634962f953c8d94754
William Boman <william@redwill.se>
2021-09-10 13:37:49 +0200

    change server root path for tailwind too

--- lua/nvim-lsp-installer/servers/tailwindcss/init.lua
+++ lua/nvim-lsp-installer/servers/tailwindcss/init.lua
@@ -1,7 +1,7 @@
 local server = require "nvim-lsp-installer.server"
 local npm = require "nvim-lsp-installer.installers.npm"
 
-local root_dir = server.get_server_root_path "tailwindcss"
+local root_dir = server.get_server_root_path "tailwindcss_npm"
 
 return server.Server:new {
     name = "tailwindcss",
