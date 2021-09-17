1a4d5e2054440be98f8e6b09ee2a0419a11ced54
William Boman <william@redwill.se>
2021-09-17 20:35:25 +0200

    update r_language_server to new structure

--- lua/nvim-lsp-installer/servers/r_language_server/init.lua
+++ lua/nvim-lsp-installer/servers/r_language_server/init.lua
@@ -1,21 +1,28 @@
-local server = require 'nvim-lsp-installer.server'
-local path = require "nvim-lsp-installer.path"
-local process = require("nvim-lsp-installer.process")
+local server = require "nvim-lsp-installer.server"
+local process = require "nvim-lsp-installer.process"
 
-local root_dir = server.get_server_root_path("r_language_server")
-
-return server.Server:new {
-    name = "r_language_server",
-    root_dir = root_dir,
-    installer = function(server, callback, context)
-        process.spawn("R", {
-          args = { "-e", 'install.packages("languageserver", repos="http://cran.us.r-project.org", lib = "' .. root_dir .. '")' },
-          stdio_sink = context.stdio_sink
-        }, callback)
-    end,
-    default_options = {
-        cmd = {
-            'R', '--slave', '-e', 'library(languageserver, lib.loc = "' .. root_dir .. '"); languageserver::run()'
-        }
+return function(name, root_dir)
+    return server.Server:new {
+        name = name,
+        root_dir = root_dir,
+        installer = function(_, callback, context)
+            process.spawn("R", {
+                args = {
+                    "-e",
+                    ('install.packages("languageserver", repos="http://cran.us.r-project.org", lib = %q)'):format(
+                        root_dir
+                    ),
+                },
+                stdio_sink = context.stdio_sink,
+            }, callback)
+        end,
+        default_options = {
+            cmd = {
+                "R",
+                "--slave",
+                "-e",
+                ("library(languageserver, lib.loc = %q); languageserver::run()"):format(root_dir),
+            },
+        },
     }
-}
+end
