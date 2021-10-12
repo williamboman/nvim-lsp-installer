029888124fe51e4f1ccd1a1bc3b287d565f20669
William Boman <william@redwill.se>
2021-10-12 16:25:19 +0200

    add context.latest_github_release

--- lua/nvim-lsp-installer/servers/erlangls/init.lua
+++ lua/nvim-lsp-installer/servers/erlangls/init.lua
@@ -1,21 +1,34 @@
 local server = require "nvim-lsp-installer.server"
 local path = require "nvim-lsp-installer.path"
-local shell = require "nvim-lsp-installer.installers.shell"
+local process = require "nvim-lsp-installer.process"
 local std = require "nvim-lsp-installer.installers.std"
-local lspconfig = require "lspconfig/util"
+local context = require "nvim-lsp-installer.installers.context"
 
 return function(name, root_dir)
-  return server.Server:new {
-    name = name,
-    root_dir = root_dir,
-    installer = {
-      shell.bash("git clone --depth 1 https://github.com/erlang-ls/erlang_ls.git . && make && cp _build/default/bin/erlang_ls ."),
-      std.chmod("+x", { "erlang_ls"} ),
-    },
-    default_options = {
-      cmd = { path.concat { root_dir, "erlang_ls" }},
-      filetypes = { "erlang" },
-      root_dir = lspconfig.root_pattern('rebar.config', 'erlang.mk', '.git') or lspconfig.path.dirname(fname)
-    },
-  }
+    return server.Server:new {
+        name = name,
+        root_dir = root_dir,
+        installer = {
+            std.ensure_executables {
+                { "rebar3", "rebar3 was not found in path. Refer to http://rebar3.org/docs/." },
+            },
+            context.latest_github_release "erlang-ls/erlang_ls",
+            std.git_clone "https://github.com/erlang-ls/erlang_ls.git",
+            function(server, callback, context)
+                local c = process.chain {
+                    cwd = server.root_dir,
+                    stdio_sink = context.stdio_sink,
+                }
+                c.run("rebar3", { "escriptize" })
+                c.run("rebar3", { "as", "dap", "escriptize" })
+                c.spawn(callback)
+            end,
+            -- TODO: check this on Windows
+            std.rename("_build/default/bin/erlang_ls", "erlang_ls"),
+            std.chmod("+x", { "erlang_ls" }),
+        },
+        default_options = {
+            cmd = { path.concat { root_dir, "erlang_ls" } },
+        },
+    }
 end
