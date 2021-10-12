8d8b1bc6eb176a925282b3c93fb65fe2832dea4d
William Boman <william@redwill.se>
2021-10-12 16:29:14 +0200

    remove installdir override

--- lua/nvim-lsp-installer/servers/init.lua
+++ lua/nvim-lsp-installer/servers/init.lua
@@ -19,7 +19,6 @@ local INSTALL_DIRS = {
     ["cssls"] = vscode_langservers_extracted "cssls",
     ["dockerls"] = "dockerfile",
     ["elixirls"] = "elixir",
-    ["erlangls"] = "erlang",
     ["elmls"] = "elm",
     ["eslintls"] = "eslint",
     ["gopls"] = "go",
