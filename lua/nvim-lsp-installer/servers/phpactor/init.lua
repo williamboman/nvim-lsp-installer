8fe2b627010ec630550cfd779cb27f9f1e5390ff
Lctrs <jerome@prmntr.me>
2021-10-17 01:13:56 +0200

    fix(phpactor): install via git clone instead of composer require
    As stated [here](https://github.com/phpactor/phpactor/issues/1322), `composer require` is not a supported way of installing phpactor.
    According to the [doc](https://phpactor.readthedocs.io/en/master/usage/standalone.html), we should rely on `git clone` instead.

--- lua/nvim-lsp-installer/servers/phpactor/init.lua
+++ lua/nvim-lsp-installer/servers/phpactor/init.lua
@@ -1,14 +1,19 @@
+local path = require "nvim-lsp-installer.path"
 local server = require "nvim-lsp-installer.server"
 local composer = require "nvim-lsp-installer.installers.composer"
+local std = require "nvim-lsp-installer.installers.std"
 
 return function(name, root_dir)
     return server.Server:new {
         name = name,
         root_dir = root_dir,
         homepage = "https://phpactor.readthedocs.io/en/master/",
-        installer = composer.packages { "phpactor/phpactor" },
+        installer = {
+            std.git_clone "https://github.com/phpactor/phpactor.git",
+            composer.install(),
+        },
         default_options = {
-            cmd = { composer.executable(root_dir, "phpactor"), "language-server" },
+            cmd = { path.concat { root_dir, "bin", "phpactor" }, "language-server" },
         },
     }
 end
