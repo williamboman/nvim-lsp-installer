6dae673a3a63e2760e2331595713d7d74250b481
Trent Nicholson <nicholsontrent@gmail.com>
2021-10-19 12:02:34 +1300

    Revert "Wrap vscode-langservers cmd's with `yarn node` to resolve dependencies in"
    This reverts commit e44416c09f24decf2a35ee2169062cdec745086e.

--- lua/nvim-lsp-installer/servers/vscode-langservers-extracted/init.lua
+++ lua/nvim-lsp-installer/servers/vscode-langservers-extracted/init.lua
@@ -1,25 +1,15 @@
 local server = require "nvim-lsp-installer.server"
 local npm = require "nvim-lsp-installer.installers.npm"
 
-local function get_command(root_dir, executable)
-    local cmd = npm.executable(root_dir, executable)
-
-    -- If we have yarn installed execute the lsp wrapped with yarn node to
-    -- avoid issues resolving modules in yarn 2 repo's
-    if vim.fn.executable('yarn') == 1 then
-        return {'yarn', 'node', cmd, '--stdio'}
-    end
-    return {cmd, '--stdio'}
-
-end
-
 return function(executable)
     return function(name, root_dir)
-        return server.Server:new{
+        return server.Server:new {
             name = name,
             root_dir = root_dir,
-            installer = npm.packages {"vscode-langservers-extracted"},
-            default_options = {cmd = get_command(root_dir, executable)}
+            installer = npm.packages { "vscode-langservers-extracted" },
+            default_options = {
+                cmd = { npm.executable(root_dir, executable), "--stdio" },
+            },
         }
     end
 end
