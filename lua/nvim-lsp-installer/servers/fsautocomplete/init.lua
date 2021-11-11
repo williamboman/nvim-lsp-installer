c5c067a12db94181bb04cd5cbf532e62d523035b
William Boman <william@redwill.se>
2021-11-11 13:44:55 +0100

    fsautocomplete: don't use shell as an intermediary
    This fixes issues on Windows (and prob more platforms) where
    the shell command may fail but this is not captured by the
    installer.

--- lua/nvim-lsp-installer/servers/fsautocomplete/init.lua
+++ lua/nvim-lsp-installer/servers/fsautocomplete/init.lua
@@ -1,7 +1,7 @@
 local server = require "nvim-lsp-installer.server"
 local path = require "nvim-lsp-installer.path"
+local process = require "nvim-lsp-installer.process"
 local std = require "nvim-lsp-installer.installers.std"
-local shell = require "nvim-lsp-installer.installers.shell"
 
 return function(name, root_dir)
     return server.Server:new {
@@ -13,7 +13,21 @@ return function(name, root_dir)
             std.ensure_executables {
                 { "dotnet", "dotnet was not found in path." },
             },
-            shell.polyshell [[dotnet tool update --tool-path . fsautocomplete]],
+            ---@type ServerInstallerFunction
+            function (_, callback, ctx)
+                process.spawn("dotnet", {
+                    args = { "tool", "update", "--tool-path", ".", "fsautocomplete" },
+                    cwd = ctx.install_dir,
+                    stdio_sink = ctx.stdio_sink,
+                }, function (success)
+                    if not success then
+                        ctx.stdio_sink.stderr("Failed to install fsautocomplete.\n")
+                        callback(false)
+                    else
+                        callback(true)
+                    end
+                end)
+            end,
         },
         default_options = {
             cmd = {
