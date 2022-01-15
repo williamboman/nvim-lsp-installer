c81d40893b27c2dc742e7789e7192b3d97df9f14
William Boman <william@redwill.se>
2022-01-15 14:32:42 +0100

    add composer.require() function, implement receipt mechanism for composer

--- lua/nvim-lsp-installer/servers/psalm/init.lua
+++ lua/nvim-lsp-installer/servers/psalm/init.lua
@@ -1,10 +1,5 @@
-local installers = require "nvim-lsp-installer.installers"
-local path = require "nvim-lsp-installer.path"
 local server = require "nvim-lsp-installer.server"
 local composer = require "nvim-lsp-installer.installers.composer"
-local std = require "nvim-lsp-installer.installers.std"
-local context = require "nvim-lsp-installer.installers.context"
-local process = require "nvim-lsp-installer.process"
 
 return function(name, root_dir)
     return server.Server:new {
@@ -12,19 +7,9 @@ return function(name, root_dir)
         root_dir = root_dir,
         homepage = "https://psalm.dev/",
         languages = { "php" },
-        installer = installers.when {
-            unix = {
-                std.git_clone "https://github.com/vimeo/psalm.git",
-                composer.install(),
-                context.receipt(function(receipt)
-                    receipt:with_primary_source(receipt.git_remote "https://github.com/vimeo/psalm.git")
-                end),
-            },
-        },
+        installer = composer.require { "vimeo/psalm" },
         default_options = {
-            cmd_env = {
-                PATH = process.extend_path { path.concat { root_dir, "bin" } },
-            },
+            cmd_env = composer.env(root_dir),
         },
     }
 end
