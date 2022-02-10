455de5e7a608b830b1756403b73322d8844b50f6
Ibrahim Abdelkareem <ibrahim.abdelkareem.1990@gmail.com>
2022-02-10 15:58:44 +0100

    fix styling

--- lua/nvim-lsp-installer/servers/omnisharp/init.lua
+++ lua/nvim-lsp-installer/servers/omnisharp/init.lua
@@ -4,6 +4,7 @@ local path = require "nvim-lsp-installer.path"
 local Data = require "nvim-lsp-installer.data"
 local std = require "nvim-lsp-installer.installers.std"
 local context = require "nvim-lsp-installer.installers.context"
+local dotnet = require "nvim-lsp-installer.installers.dotnet"
 
 local coalesce, when = Data.coalesce, Data.when
 
@@ -17,10 +18,11 @@ return function(name, root_dir)
             context.use_github_release_file(
                 "OmniSharp/omnisharp-roslyn",
                 coalesce(
-                    when(platform.is_mac,
+                    when(
+                        platform.is_mac,
                         coalesce(
-                          when(platform.arch == "x64", "omnisharp-osx-x64-net6.0.zip"),
-                          when(platform.arch == "arm64", "omnisharp-osx-arm64-net6.0.zip")
+                            when(platform.arch == "x64", "omnisharp-osx-x64-net6.0.zip"),
+                            when(platform.arch == "arm64", "omnisharp-osx-arm64-net6.0.zip")
                         )
                     ),
                     when(
