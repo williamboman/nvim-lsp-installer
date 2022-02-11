fd9b73b8e1bd990923d31071569da4a4076c1797
William Boman <william@redwill.se>
2022-02-11 01:04:43 +0100

    extract R scripts to variables

--- lua/nvim-lsp-installer/servers/r_language_server/init.lua
+++ lua/nvim-lsp-installer/servers/r_language_server/init.lua
@@ -2,30 +2,39 @@ local server = require('nvim-lsp-installer.server')
 local process = require('nvim-lsp-installer.process')
 
 return function(name, root_dir)
+  local install_script = [[
+options(langserver_library = ".");
+rlsLib = getOption("langserver_library");
+install.packages("languageserversetup", lib = rlsLib);
+loadNamespace("languageserversetup", lib.loc = rlsLib);
+
+languageserversetup::languageserver_install(
+    fullReinstall = FALSE,
+    confirmBeforeInstall = FALSE,
+    strictLibrary = FALSE,
+    libs_only = TRUE
+);
+]]
+
+  local server_script = ([[
+options("langserver_library" = %q);
+rlsLib = getOption("langserver_library");
+.libPaths(new = rlsLib);
+loadNamespace("languageserver", lib.loc = rlsLib);
+languageserver::run();
+  ]]):format(root_dir)
+
   return server.Server:new({
     name = name,
     root_dir = root_dir,
-    installer = function(_, callback, context)
+    installer = function(_, callback, ctx)
       process.spawn('R', {
+        cwd = ctx.install_dir,
         args = {
           '-e',
-          ('options(langserver_library = %q)'):format(root_dir)
-            .. ';'
-            .. 'rlsLib = getOption("langserver_library")'
-            .. ';'
-            .. 'install.packages("languageserversetup", lib = rlsLib)'
-            .. ';'
-            .. 'loadNamespace("languageserversetup", lib.loc = rlsLib)'
-            .. ';'
-            .. [[languageserversetup::languageserver_install(
-              fullReinstall = FALSE,
-              confirmBeforeInstall = FALSE,
-              strictLibrary = FALSE,
-              libs_only = TRUE
-            )]],
+          install_script,
         },
-        --
-        stdio_sink = context.stdio_sink,
+        stdio_sink = ctx.stdio_sink,
       }, callback)
     end,
     default_options = {
@@ -33,15 +42,7 @@ return function(name, root_dir)
         'R',
         '--slave',
         '-e',
-        ('options("langserver_library" = %q)'):format(root_dir)
-          .. ';'
-          .. 'rlsLib = getOption("langserver_library")'
-          .. ';'
-          .. '.libPaths(new = rlsLib)'
-          .. ';'
-          .. 'loadNamespace("languageserver", lib.loc = rlsLib)'
-          .. ';'
-          .. 'languageserver::run()',
+        server_script
       },
     },
   })
