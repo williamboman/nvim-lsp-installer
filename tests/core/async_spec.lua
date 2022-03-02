7f2f90ffa5b5a0e4d4c9180eb9df96777c1ee537
William Boman <william@redwill.se>
2022-03-02 20:09:50 +0100

    fix(async): pcall callback-style functions to catch errors

--- tests/core/async_spec.lua
+++ tests/core/async_spec.lua
@@ -1,5 +1,6 @@
 local assert = require "luassert"
 local spy = require "luassert.spy"
+local match = require "luassert.match"
 local a = require "nvim-lsp-installer.core.async"
 local process = require "nvim-lsp-installer.process"
 
@@ -45,6 +46,17 @@ describe("async", function()
             poutine()
             a.sleep(200)
             assert.spy(james_bond).was_not.called()
+        end)
+    )
+
+    it(
+        "should reject if async function raises error",
+        async_test(function()
+            local ok, err = a.promisify(function()
+                error "something went wrong"
+            end)()
+            assert.is_false(ok)
+            assert.is_true(match.has_match "something went wrong$"(err))
         end)
     )
 end)
