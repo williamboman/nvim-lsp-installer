033660f2762d482b9c447d8af5cb063ab0895f56
William Boman <william@redwill.se>
2022-05-01 15:44:53 +0200

    Apply suggestions from code review

--- lua/nvim-lsp-installer/servers/beancount/init.lua
+++ lua/nvim-lsp-installer/servers/beancount/init.lua
@@ -1,8 +1,5 @@
 local server = require "nvim-lsp-installer.server"
-local Data = require "nvim-lsp-installer.data"
 local cargo = require "nvim-lsp-installer.core.managers.cargo"
-
-local coalesce, when = Data.coalesce, Data.when
 
 return function(name, root_dir)
     return server.Server:new {
@@ -11,9 +8,9 @@ return function(name, root_dir)
         languages = { "beancount" },
         homepage = "https://github.com/polarmutex/beancount-language-server",
         async = true,
-        installer = cargo.crate("beancount-language-server"),
+        installer = cargo.crate "beancount-language-server",
         default_options = {
-            cmd_env = cargo.env(root_dir)
+            cmd_env = cargo.env(root_dir),
         },
     }
 end
