0a42c21caf179590dbf3fbe9e3f21fd942d49fa1
William Boman <william@redwill.se>
2022-05-13 17:39:12 +0200

    fix(vls): download prebuilt binaries (per recommendation)

--- lua/nvim-lsp-installer/servers/vls/init.lua
+++ lua/nvim-lsp-installer/servers/vls/init.lua
@@ -1,50 +1,44 @@
-local lspconfig = require "lspconfig"
-local configs = require "lspconfig.configs"
-local servers = require "nvim-lsp-installer.servers"
 local server = require "nvim-lsp-installer.server"
-local path = require "nvim-lsp-installer.path"
-local shell = require "nvim-lsp-installer.installers.shell"
-
+local path = require "nvim-lsp-installer.core.path"
+local github = require "nvim-lsp-installer.core.managers.github"
+local github_client = require "nvim-lsp-installer.core.managers.github.client"
+local std = require "nvim-lsp-installer.core.managers.std"
+local functional = require "nvim-lsp-installer.core.functional"
+local platform = require "nvim-lsp-installer.core.platform"
+local Optional = require "nvim-lsp-installer.core.optional"
+local process = require "nvim-lsp-installer.core.process"
 
 return function(name, root_dir)
-    local server_name = "vls"
-
-    local root_dir = server.get_server_root_path(server_name)
-
-    local vls_installer = shell.bash [[
-# Must be cloned with git, since the VLS installer requires it
-# See build.vsh in vlang/vls
-    git clone https://github.com/vlang/vls.git && cd vls
-
-# Build with v
-    if type clang
-    then
-        v run build.vsh clang
-    elif type gcc
-    then
-        v run build.vsh gcc
-    elif type cc
-    then
-        v run build.vsh cc
-    elif type msvc
-    then
-        v run build.vsh cc
-    else
-        echo "Failed to find a compiler"
-        exit 1
-    fi
-]]
-
-    -- 2. (mandatory) Create an nvim-lsp-installer Server instance
-    local vls_server = server.Server:new {
-        name = server_name,
+    return server.Server:new {
+        name = name,
         root_dir = root_dir,
         homepage = "https://github.com/vlang/vls",
-        languages = {"vlang"},
-        installer = vls_installer,
+        languages = { "vlang" },
+        ---@async
+        installer = function()
+            local repo = "vlang/vls"
+
+            ---@type GitHubRelease
+            local latest_dev_build =
+                github_client.fetch_latest_release(repo, { include_prelease = true }):get_or_throw()
+
+            local source = github.release_file {
+                version = Optional.of(latest_dev_build.tag_name),
+                repo = repo,
+                asset_file = functional.coalesce(
+                    functional.when(platform.is.linux_x64, "vls_linux_x64"),
+                    functional.when(platform.is.mac, "vls_macos_x64"),
+                    functional.when(platform.is.win_x64, "vls_windows_x64.exe")
+                ),
+            }
+            source.with_receipt()
+            std.download_file(source.download_url, platform.is.win and "vls.exe" or "vls")
+            std.chmod("+x", { "vls" })
+        end,
         default_options = {
-            cmd = { path.concat { root_dir, "vls/bin/vls" } },
+            cmd_env = {
+                PATH = process.extend_path { root_dir },
+            },
         },
     }
-    return vls_server
 end
