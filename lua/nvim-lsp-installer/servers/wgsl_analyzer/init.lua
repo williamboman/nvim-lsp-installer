94914912b22f72043c0d327c156e43c9b9cb3592
skwee357 <ko.dmitri@gmail.com>
2022-05-25 20:04:31 +0300

    moved wgsl_analyzer config to lspconfig repo

--- lua/nvim-lsp-installer/servers/wgsl_analyzer/init.lua
+++ lua/nvim-lsp-installer/servers/wgsl_analyzer/init.lua
@@ -1,18 +1,5 @@
 local server = require "nvim-lsp-installer.server"
 local cargo = require "nvim-lsp-installer.core.managers.cargo"
-local lspconfig = require "lspconfig"
-local configs = require "lspconfig.configs"
-
-if not configs.wgsl_analyzer then
-    configs.wgsl_analyzer = {
-        default_config = {
-            cmd = { "wgsl_analyzer" },
-            filetypes = { "wgsl" },
-            root_dir = lspconfig.util.root_pattern(".git", "wgsl"),
-            settings = {}
-        }
-    }
-end
 
 return function(name, root_dir)
     local homepage = "https://github.com/wgsl-analyzer/wgsl-analyzer"
