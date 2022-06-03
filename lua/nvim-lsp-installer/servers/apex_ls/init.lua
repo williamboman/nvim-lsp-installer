0aa984e861274a3dd8b9713e43662dae96e4b780
William Boman <william@redwill.se>
2022-06-03 18:36:15 +0200

    extract jar file

--- lua/nvim-lsp-installer/servers/apex_ls/init.lua
+++ lua/nvim-lsp-installer/servers/apex_ls/init.lua
@@ -5,25 +5,31 @@ local Optional = require "nvim-lsp-installer.core.optional"
 local path = require "nvim-lsp-installer.core.path"
 
 return function(name, root_dir)
+    local JAR_FILE = "apex-jorje-lsp.jar"
+
     return server.Server:new {
         name = name,
         root_dir = root_dir,
         homepage = "https://github.com/forcedotcom/salesforcedx-vscode",
         languages = { "apex" },
         ---@async
-        installer = function()
+        ---@param ctx InstallContext
+        installer = function(ctx)
             local source = github.tag { repo = "forcedotcom/salesforcedx-vscode" }
             source.with_receipt()
             git.clone { "https://github.com/forcedotcom/salesforcedx-vscode", version = Optional.of(source.tag) }
+            ctx.fs:rename(
+                path.concat {
+                    "packages",
+                    "salesforcedx-vscode-apex",
+                    "out",
+                    JAR_FILE,
+                },
+                JAR_FILE
+            )
         end,
         default_options = {
-            apex_jar_path = path.concat {
-                root_dir,
-                "packages",
-                "salesforcedx-vscode-apex",
-                "out",
-                "apex-jorje-lsp.jar",
-            },
+            apex_jar_path = path.concat { root_dir, JAR_FILE },
         },
     }
 end
