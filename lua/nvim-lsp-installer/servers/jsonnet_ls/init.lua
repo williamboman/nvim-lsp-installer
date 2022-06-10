0207d408ebefe5677fe3068f261a4b98c2e79102
William Boman <william@redwill.se>
2022-06-10 15:58:00 +0200

    fix(jsonnet_ls): use cmd_env instead of overriding cmd

--- lua/nvim-lsp-installer/servers/jsonnet_ls/init.lua
+++ lua/nvim-lsp-installer/servers/jsonnet_ls/init.lua
@@ -1,4 +1,3 @@
-local path = require "nvim-lsp-installer.core.path"
 local server = require "nvim-lsp-installer.server"
 local go = require "nvim-lsp-installer.core.managers.go"
 
@@ -9,8 +8,7 @@ return function(name, root_dir)
         homepage = "https://github.com/grafana/jsonnet-language-server",
         installer = go.packages { "github.com/grafana/jsonnet-language-server" },
         default_options = {
-            -- TODO: use env instead of cmd once https://github.com/neovim/nvim-lspconfig/pull/1559 is merged
-            cmd = { path.concat { root_dir, "jsonnet-language-server" } },
+            cmd_env = go.env(root_dir),
         },
     }
 end
