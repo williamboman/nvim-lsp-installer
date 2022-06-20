8c6fe2152bb0c7f609d53e0ea589c9a167f2162c
Davide Pizzolotto <davide.pizzolotto@gmail.com>
2022-06-20 17:00:00 +0900

    fix(texlab): download aarch64 assets on arm macOS

--- lua/nvim-lsp-installer/servers/texlab/init.lua
+++ lua/nvim-lsp-installer/servers/texlab/init.lua
@@ -19,7 +19,8 @@ return function(name, root_dir)
                     github.untargz_release_file({
                         repo = repo,
                         asset_file = coalesce(
-                            when(platform.is_mac, "texlab-x86_64-macos.tar.gz"),
+                            when(platform.is_mac and platform.arch == "arm64", "texlab-aarch64-macos.tar.gz"),
+                            when(platform.is_mac and platform.arch == "x64", "texlab-x86_64-macos.tar.gz"),
                             when(platform.is_linux and platform.arch == "x64", "texlab-x86_64-linux.tar.gz")
                         ),
                     }).with_receipt()
